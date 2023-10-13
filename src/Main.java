21de0ca35aad4b5827d63e57211c30a48d2c6bf6
Aleksandr K <alkaz05@mail.ru>
2023-10-13 14:59:08 +0300

    исправлена (?) ошибка в сортировке

--- src/Main.java
+++ src/Main.java
@@ -4,13 +4,20 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
+    static int k=0;
+
     public static void main(String[] args) throws FileNotFoundException {
-        int[] arr = readArrayFromF("array.txt");
+        int[] arr = readArrayFromF("INPUT");
         System.out.println(Arrays.toString(arr));
         //bubbleSort(arr);
         quickSort(arr, 0, arr.length-1);
         System.out.println(Arrays.toString(arr));
+        System.out.println(k);
     }
+
+    //сделать функцию, которая создает массив случайных целых чисел
+    //диапазона от 0 до 99. Аргумент функции - длина массива
+
 
     private static int[] readArrayFromF(String fileName) throws FileNotFoundException {
         Scanner scan = new Scanner(new File(fileName));
@@ -26,7 +33,7 @@ public class Main {
         if(start>=finish)
             return;
         int p = partition(mas, start, finish);
-        quickSort(mas, start, p);
+        quickSort(mas, start, p-1);
         quickSort(mas, p+1, finish);
     }
 
@@ -36,14 +43,20 @@ public class Main {
         int j=finish;
         int tmp;
         while (true){
+            k++;
             while(i<=finish && mas[i]<pivot )
-                i++;
+            {    i++;   k++;}
+            k++;
             while (j>=start && mas[j]>pivot )
-                j--;
+            {    j--;   k++;}
             if(i<j) {
-                tmp = mas[i];
-                mas[i] = mas[j];
-                mas[j] = tmp;
+                if(mas[i] == mas[j])
+                    i++;
+                else {
+                    tmp = mas[i];
+                    mas[i] = mas[j];
+                    mas[j] = tmp;
+                }
             }
             else
                 return j;
